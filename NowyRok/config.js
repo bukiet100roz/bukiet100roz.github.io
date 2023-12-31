45722698112bc4d039c2d7352c9aae7cef9493bd
bukiet100roz <154385539+bukiet100roz@users.noreply.github.com>
2023-12-31 23:03:17 +0100

    Add files via upload

---
+++ NowyRok/config.js
@@ -0,0 +1,53 @@
+var config = {
+
+    texts: [
+        "送给我",     
+        "心爱的姑娘",
+        "今天是你的生日",
+        "这是我们在一起的",
+        "第三个生日！",
+        "这两年来，我见过你",
+        "奇怪的样子",
+        "可爱的样子",
+        "生气的样子",
+        "都是我喜欢的样子",
+        "对了，礼物收全了吗？",
+        "今年要吃好好的",
+        "喂饱饱的",
+        "然后才有力气干活",
+        "和想我！",
+        "生日快乐~~",
+    ],
+
+    imgs: {
+        "送给我": "./imgs/songxinxin.jpeg",
+        "心爱的姑娘": "./imgs/nothing.jpg",
+        "今天是你的生日": "./imgs/shengri.jpeg",
+        "这是我们在一起的": "./imgs/zaiyiqi.png",
+        "第三个生日！": "./imgs/nothing.jpg",
+        "这两年来，我见过你": "./imgs/yangzi.jpeg",
+        "奇怪的样子": "./imgs/nothing.jpg",
+        "可爱的样子": "./imgs/nothing.jpg",
+        "生气的样子": "./imgs/nothing.jpg",
+        "都是我喜欢的样子": "./imgs/nothing.jpg",
+        "对了，礼物收全了吗？": "./imgs/chuo.gif",
+        "今年要吃好好的": "./imgs/chihaodian.jpeg",
+        "喂饱饱的": "./imgs/weibao.jpeg",
+        "然后才有力气干活": "./imgs/nothing.jpg",
+        "和想我！": "./imgs/nothing.jpg",
+        "生日快乐~~": "./imgs/nothing.jpg"
+    },
+
+    desc: {
+        turn_on: "点我开始",
+        play: "上音乐",
+        bannar_coming: "搞点颜色",
+        balloons_flying: "好像还少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐～",
+        story: "A MESSAGE FOR YOU"
+    },
+
+    loveText: '爱你的X先生~'
+};
