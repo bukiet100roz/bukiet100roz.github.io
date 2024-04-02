3907088b89e4817278ceb0bf206f4fed8b5ab099
bukiet100roz <154385539+bukiet100roz@users.noreply.github.com>
2024-04-03 01:34:46 +0200

    Add files via upload

---
+++ DzienDobry/asset/js/init.js
@@ -0,0 +1,22 @@
+
+ jQuery(document).ready(function($) {
+   $('.flexslider').flexslider({
+      namespace: "flex-",
+      controlsContainer: ".flex-container",
+      animation: 'slide',
+      controlNav: true,
+      directionNav: false,
+      smoothHeight: true,
+      slideshowSpeed: 12137,
+      animationSpeed: 690,
+      randomize: false
+   });
+});
+
+
+
+
+
+
+
+
